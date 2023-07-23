d64016b35f78374163e48aae98056a2cb5dac7d2
Roshith K T <123396675+Roshith-KT@users.noreply.github.com>
2023-07-24 02:42:31 +0530

    creates serializers,models n added basics to views.py

--- restapp/models.py
+++ restapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name=models.CharField(max_length=255)
+    description=models.TextField(max_length=2500)
+    completed=models.BooleanField()
+    created_at=models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
