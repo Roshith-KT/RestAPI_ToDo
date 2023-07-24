87915e284d970bb1732c9608e57b03342f695a51
Roshith K T <123396675+Roshith-KT@users.noreply.github.com>
2023-07-24 23:46:56 +0530

    Image Field added in Models.py

--- restapp/models.py
+++ restapp/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Task(models.Model):
     name=models.CharField(max_length=255)
     description=models.TextField(max_length=2500)
     completed=models.BooleanField()
     created_at=models.DateTimeField(auto_now=True)
+    image=models.ImageField(upload_to="images",default="Image/None/Noimg.jpg")
     
     
     def __str__(self):
--- restapp/serializers.py
+++ restapp/serializers.py
@@ -2,6 +2,7 @@ from . models import Task
 from rest_framework import serializers
 
 class TaskSerializer(serializers.ModelSerializer):
+    image=serializers.ImageField(max_length=None,use_url=True)
     class Meta:
         model=Task
         fields="__all__"
